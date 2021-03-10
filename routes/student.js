444b1ceb98ce9743ac54d904b58802dbd479ce47
sagar110599 <sagardama99@gmail.com>
2021-03-10 10:59:45 +0530

    update data

--- routes/student.js
+++ routes/student.js
@@ -32,11 +32,48 @@ router.post('/upload-data', bodyParser.json(),upload.single('file'),function (re
     fs.unlinkSync(path.join(__basedir, "/uploads/" , req.file.filename));
     res.redirect('/')
 })
+router.post('/update-data', bodyParser.json(),upload.single('file'),function (req, res) {
+    if(req.file){
+        console.log("DDAAAAMMMMMAAAA");
+        db.User.findOne({_id:req.body.id},function(err,item){
+            item.fname=req.body.fname;
+            item.lname=req.body.lname;
+            item.email=req.body.email;
+            item.age=req.body.age;
+            item.img.data=fs.readFileSync(path.join(__basedir, "/uploads/" , req.file.filename))
+            item.save();
+        })
+        fs.unlinkSync(path.join(__basedir, "/uploads/" , req.file.filename));
+        res.redirect('/')
+
+    }
+    else{
+        console.log("NO file uploaded");
+        db.User.findOne({_id:req.body.id},function(err,item){
+            item.fname=req.body.fname;
+            item.lname=req.body.lname;
+            item.email=req.body.email;
+            item.age=req.body.age;
+            
+            item.save();
+        })
+        res.redirect('/')
+
+    }
+    
+})
 router.get('/display',function(req,res){
 
     db.User.find({},function(err,item){
         console.log(item);
         res.render('display.ejs',{items:item});
+    })
+})
+router.get('/student_data/:id',function(req,res){
+    //res.send(req.params.id);
+    db.User.findOne({_id:req.params.id},function(err,item){
+        console.log(item);
+        res.render("display_student.ejs",{item:item});
     })
 })
 router.get('/', function (req, res) {
